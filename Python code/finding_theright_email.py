a42ad1c14fb780bc75317f5da7e51617b3d2a79c
canbaran <can_barantr@yahoo.com>
2013-03-18 14:51:51 +0200

    CanB
    Yazdigim butun python kodlarini ve ornek xls dosyalarini koydum buraya

---
+++ Python code/finding_theright_email.py
@@ -0,0 +1,31 @@
+import email, getpass, imaplib, os
+
+#create mail object instance
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login('canbaran85@gmail.com', 'mclord')
+mail.select('inbox')
+[result, data] = mail.uid('search', None, '(from bogac.ayvaz@yapikredi.com.tr X-GM-RAW has:attachment)')
+data = data[0].split(' ')
+data = [int(x) for x in data]
+for x in data:
+    [resp, subdata ] = mail.uid('fetch', x, '(RFC822)')
+    email_body = subdata[0][1]
+    submail = email.message_from_string(email_body)
+    detach_dir = os.getcwd()
+    for part in submail.walk():
+        if part.get_content_maintype() == 'multipart':
+                continue
+        if part.get('Content-Disposition') is None:
+                continue
+        filename = part.get_filename()
+        [dummy, fileExtension]= os.path.splitext(filename)
+        att_path = os.path.join(detach_dir, filename)
+        if  (fileExtension in ('.xls', '.xlsx') ):
+            print filename
+            fp = open(att_path, 'wb')
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+            print 'file written'
+
+mail.close()
+mail.logout()
